620c5d32d5821791907782a8f7aaccb90cc9c60e
MelvinDukles <52915865+MelvinDukles@users.noreply.github.com>
2019-07-22 17:30:04 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,7 +1,4 @@
-num1 = int(input())
-num2 = int(input())
-num3 = int(input())
- 
+num1,num2,num3=input().split()
 if (num1 > num2) and (num1 > num3):
    largest = num1
 elif (num2 > num1) and (num2 > num3):
@@ -9,4 +6,4 @@ elif (num2 > num1) and (num2 > num3):
 else:
    largest = num3
  
-print(largest)
+print('',largest)
