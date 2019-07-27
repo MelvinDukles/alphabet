fded0f0dd24802c430835be70304953b4900ade3
MelvinDukles <52915865+MelvinDukles@users.noreply.github.com>
2019-07-27 08:49:36 +0530

    Update Arithmetic.c

--- Arithmetic.c
+++ Arithmetic.c
@@ -1,18 +1,12 @@
-{
+#include <stdio.h>
+
+int main() {
     
-    int a, n, d, tn, i;
+    int A, N, D;
     int sum = 0;
     scanf("%d %d %d",&N,&A,&D);
     
-    sum = (n * (2 * a + (n - 1) * d)) / 2;
-    tn = a + (n - 1) * d;
-    for(i = a; i <= tn; i = i + d)
-    {
-        if(i != tn)
-            printf("%d + ", i);
-        else
-            printf("%d = %d", i, sum);
-    }
-    printf("\n");
+    sum = (N * (2 * A + (N - 1) * D)) / 2;
+    printf("%d",sum);
     return 0;
 }
